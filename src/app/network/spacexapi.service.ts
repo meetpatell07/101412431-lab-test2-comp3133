bd2aabdbd09fac8bbee108c298b13112b0fdc299
Pmack0702 <meetpatel7026@gmail.com>
2025-04-02 06:19:11 -0700

    Mission filter added

---
+++ src/app/network/spacexapi.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SpaceXApiService {
+  private apiUrl = 'https://api.spacexdata.com/v3/launches';
+
+  constructor(private http: HttpClient) {}
+
+  getAllMissions(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  getMissionsByYear(year: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}?launch_year=${year}`);
+  }
+}
